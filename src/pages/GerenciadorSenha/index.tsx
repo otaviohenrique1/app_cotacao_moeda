58d2e981e6dcb751cefc19538055d43e419e2cb2
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-09-20 12:57:55 -0300

    commit 29

--- src/pages/GerenciadorSenha/index.tsx
+++ src/pages/GerenciadorSenha/index.tsx
@@ -1,42 +1,80 @@
 import Constants from "expo-constants";
 import { useState } from "react";
-import { Alert, Button, TextInput } from "react-native";
+import { Button, ScrollView, TextInput } from "react-native";
 import { StyleSheet, Text, View } from "react-native";
 import { StatusBar } from 'expo-status-bar';
 import * as yup from "yup";
 import { Formik } from 'formik';
+import { Picker } from "@react-native-picker/picker";
 
 const validationSchema = yup.object().shape({
   tamanhoSenha: yup
     .number()
     .min(8, "Minimo de 8 caracteres")
     .required('O campo é obrigatório'),
+  tipo: yup
+    .string()
+    .required('O campo é obrigatório'),
 });
 
 interface FormTypes {
   tamanhoSenha: number;
+  tipo: string;
 }
 
 const valoresIniciais: FormTypes = {
   tamanhoSenha: 0,
+  tipo: "",
 };
 
 export default function GeradorSenha() {
   const [senha, setSenha] = useState("");
 
-  const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ!@#$%^&*()+?><:{}[]";
+  function gerenciadorSenha(tamanhoSenha: number, tipo: string) {
+    let senha = "";
+    let charsLista = "";
+    switch (tipo) {
+      case "charsTodos":
+        charsLista = charsTodos;
+        break;
 
-  function gerenciadorSenha(tamanhoSenha: number) {
-    let password = "";
-    for (var i = 0; i < tamanhoSenha; i++) {
-      var randomNumber = Math.floor(Math.random() * chars.length);
-      password += chars.substring(randomNumber, randomNumber + 1);
+      case "charsLetrasNumeros":
+        charsLista = charsLetrasNumeros;
+        break;
+
+      case "charsLetras":
+        charsLista = charsLetras;
+        break;
+
+      case "charsEspeciais":
+        charsLista = charsEspeciais;
+        break;
+
+      case "charsNumeros":
+        charsLista = charsNumeros;
+        break;
+
+      case "charsMaiusculo":
+        charsLista = charsMaiusculo;
+        break;
+
+      case "charsMinusculo":
+        charsLista = charsMinusculo;
+        break;
+
+      default:
+        charsLista = charsTodos;
+        break;
     }
-    return password;
+    for (var i = 0; i < tamanhoSenha; i++) {
+      var randomNumber = Math.floor(Math.random() * charsLista.length);
+      senha += charsLista.substring(randomNumber, randomNumber + 1);
+    }
+    return senha;
   }
-  
+
   function onSubmitForm(values: FormTypes) {
-    const texto = gerenciadorSenha(values.tamanhoSenha);
+    const texto = gerenciadorSenha(values.tamanhoSenha, values.tipo);
     setSenha(texto);
   }
 
@@ -45,40 +83,61 @@ export default function GeradorSenha() {
       <View style={styles.header}>
         <Text style={styles.titulo}>Gerador de Senha</Text>
       </View>
-      <View style={styles.container}>
-        <Formik
-          initialValues={valoresIniciais}
-          validationSchema={validationSchema}
-          onSubmit={onSubmitForm}
-          >
-          {({ handleChange, handleBlur, handleSubmit, setFieldValue, resetForm, values, errors }) => (
-            <View>
-              <View style={styles.senhaContainer}>
-                <Text>Resultado</Text>
-                <Text style={styles.senha}>{senha}</Text>
-              </View>
-              <View style={{ marginBottom: 16 }}>
-                <Text>Tamanho da senha</Text>
-                <TextInput
-                  onChangeText={handleChange('tamanhoSenha')}
-                  onBlur={handleBlur('tamanhoSenha')}
-                  value={(values.tamanhoSenha).toString()}
-                  style={[styles.input]}
-                  placeholder="Quantidade de caracteres"
-                  keyboardType="numeric"
-                />
-                {errors.tamanhoSenha && <Text style={{ color: "red" }}>{errors.tamanhoSenha}</Text>}
-              </View>
-              <Button title="Gerar" color="blue" onPress={() => handleSubmit()} />
-              <Button title="Limpar" color="red" onPress={() => {
-                setSenha("");
-                resetForm();
-              }} />
-            </View>
-          )}
-        </Formik>
-      </View>
       <StatusBar style="dark" backgroundColor="cadetblue" />
+      <ScrollView>
+        <View style={styles.container}>
+          <Formik
+            initialValues={valoresIniciais}
+            validationSchema={validationSchema}
+            onSubmit={onSubmitForm}
+          >
+            {({ handleChange, handleBlur, handleSubmit, setFieldValue, resetForm, values, errors }) => (
+              <View>
+                <View style={styles.senhaContainer}>
+                  <Text>Resultado</Text>
+                  <Text style={styles.senha}>
+                    {(senha === "") ? "********" : senha}
+                  </Text>
+                </View>
+                <View style={{ marginBottom: 16 }}>
+                  <Text>Tamanho da senha</Text>
+                  <TextInput
+                    onChangeText={handleChange('tamanhoSenha')}
+                    onBlur={handleBlur('tamanhoSenha')}
+                    value={(values.tamanhoSenha).toString()}
+                    style={[styles.input]}
+                    placeholder="Quantidade de caracteres"
+                    keyboardType="numeric"
+                  />
+                  {errors.tamanhoSenha && <Text style={{ color: "red" }}>{errors.tamanhoSenha}</Text>}
+                </View>
+                <View style={{ marginBottom: 16 }}>
+                  <Text>Tipo</Text>
+                  <View style={styles.selectContainer}>
+                    <Picker
+                      selectedValue={values.tipo}
+                      onValueChange={(itemValue, itemIndex) => setFieldValue("tipo", itemValue)}
+                      style={styles.select}
+                    >
+                      {listaTipo.map((item, index) => {
+                        return (
+                          <Picker.Item label={item.label} value={item.value} key={index} />
+                        );
+                      })}
+                    </Picker>
+                  </View>
+                  {errors.tipo && <Text style={{ color: "red" }}>{errors.tipo}</Text>}
+                </View>
+                <Button title="Gerar" color="blue" onPress={() => handleSubmit()} />
+                <Button title="Limpar" color="red" onPress={() => {
+                  setSenha("");
+                  resetForm();
+                }} />
+              </View>
+            )}
+          </Formik>
+        </View>
+      </ScrollView>
     </>
   );
 }
@@ -100,8 +159,6 @@ const styles = StyleSheet.create({
     flexDirection: "column"
   },
   input: {
-    // height: 40,
-    // marginVertical: 12,
     borderWidth: 1,
     padding: 10,
   },
@@ -112,5 +169,34 @@ const styles = StyleSheet.create({
   },
   senha: {
     textAlign: "center",
-  }
+    fontSize: 22,
+  },
+  selectContainer: {
+    borderWidth: 1,
+    // marginBottom: 16,
+  },
+  select: {
+    width: "100%",
+    // width: 300,
+    height: 50,
+  },
 });
+
+const listaTipo = [
+  { label: "Selecione", value: "" },
+  { label: "Todos", value: "charsTodos" },
+  { label: "Letras e Numeros", value: "charsLetrasNumeros" },
+  { label: "Letras", value: "charsLetras" },
+  { label: "Especiais", value: "charsEspeciais" },
+  { label: "Numeros", value: "charsNumeros" },
+  { label: "Maiusculo", value: "charsMaiusculo" },
+  { label: "Minusculo", value: "charsMinusculo" },
+];
+
+const charsTodos = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ!@#$%^&*()+?><:{}[]";
+const charsLetrasNumeros = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ";
+const charsLetras = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ";
+const charsEspeciais = "!@#$%^&*()+?><:{}[]";
+const charsNumeros = "0123456789";
+const charsMaiusculo = "ABCDEFGHIJLMNOPQRSTUVWXYZ";
+const charsMinusculo = "abcdefghijklmnopqrstuvwxyz";
