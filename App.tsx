dc233c54121e4437bc6851b0b73724ef72c5b775
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-09-20 11:39:32 -0300

    commit 28

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import Home from './src/pages/Home';
 import Home2 from './src/pages/Home2';
 import Home3 from './src/pages/Home3';
+import GeradorSenha from './src/pages/GerenciadorSenha';
 
 export default function App() {
   return (
-    <Home />
+    <GeradorSenha />
   );
 }
--- src/pages/GerenciadorSenha/index.tsx
+++ src/pages/GerenciadorSenha/index.tsx
@@ -1,15 +1,84 @@
 import Constants from "expo-constants";
+import { useState } from "react";
+import { Alert, Button, TextInput } from "react-native";
 import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import * as yup from "yup";
+import { Formik } from 'formik';
 
-export function GerenciadorSenha() {
+const validationSchema = yup.object().shape({
+  tamanhoSenha: yup
+    .number()
+    .min(8, "Minimo de 8 caracteres")
+    .required('O campo é obrigatório'),
+});
+
+interface FormTypes {
+  tamanhoSenha: number;
+}
+
+const valoresIniciais: FormTypes = {
+  tamanhoSenha: 0,
+};
+
+export default function GeradorSenha() {
+  const [senha, setSenha] = useState("");
+
+  const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ!@#$%^&*()+?><:{}[]";
+
+  function gerenciadorSenha(tamanhoSenha: number) {
+    let password = "";
+    for (var i = 0; i < tamanhoSenha; i++) {
+      var randomNumber = Math.floor(Math.random() * chars.length);
+      password += chars.substring(randomNumber, randomNumber + 1);
+    }
+    return password;
+  }
+  
+  function onSubmitForm(values: FormTypes) {
+    const texto = gerenciadorSenha(values.tamanhoSenha);
+    setSenha(texto);
+  }
+
   return (
     <>
       <View style={styles.header}>
-        <Text style={styles.titulo}>App cotação de moeda</Text>
+        <Text style={styles.titulo}>Gerador de Senha</Text>
       </View>
-      <View>
-        <Text>GerenciadorSenha</Text>
+      <View style={styles.container}>
+        <Formik
+          initialValues={valoresIniciais}
+          validationSchema={validationSchema}
+          onSubmit={onSubmitForm}
+          >
+          {({ handleChange, handleBlur, handleSubmit, setFieldValue, resetForm, values, errors }) => (
+            <View>
+              <View style={styles.senhaContainer}>
+                <Text>Resultado</Text>
+                <Text style={styles.senha}>{senha}</Text>
+              </View>
+              <View style={{ marginBottom: 16 }}>
+                <Text>Tamanho da senha</Text>
+                <TextInput
+                  onChangeText={handleChange('tamanhoSenha')}
+                  onBlur={handleBlur('tamanhoSenha')}
+                  value={(values.tamanhoSenha).toString()}
+                  style={[styles.input]}
+                  placeholder="Quantidade de caracteres"
+                  keyboardType="numeric"
+                />
+                {errors.tamanhoSenha && <Text style={{ color: "red" }}>{errors.tamanhoSenha}</Text>}
+              </View>
+              <Button title="Gerar" color="blue" onPress={() => handleSubmit()} />
+              <Button title="Limpar" color="red" onPress={() => {
+                setSenha("");
+                resetForm();
+              }} />
+            </View>
+          )}
+        </Formik>
       </View>
+      <StatusBar style="dark" backgroundColor="cadetblue" />
     </>
   );
 }
@@ -23,5 +92,25 @@ const styles = StyleSheet.create({
   },
   titulo: {
     fontSize: 20,
+  },
+  container: {
+    padding: 16,
+    flex: 1,
+    justifyContent: "space-between",
+    flexDirection: "column"
+  },
+  input: {
+    // height: 40,
+    // marginVertical: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  senhaContainer: {
+    marginVertical: 8,
+    borderWidth: 1,
+    padding: 10,
+  },
+  senha: {
+    textAlign: "center",
   }
 });
